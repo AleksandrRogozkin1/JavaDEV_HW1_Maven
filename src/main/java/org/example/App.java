e5228e0f90112afa9514e8740c1ad9b34f3389d6
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-04-26 19:05:32 +0300

    a

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import com.google.gson.Gson;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        User user=new User("Oleksandr","Rogozkin");
+        System.out.println(new Gson().toJson(user));
     }
 }
--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,2 +1,12 @@
-package org.example;public class User {
+package org.example;
+
+public class User {
+    String name;
+    String surname;
+
+
+    public User(String name, String surname) {
+        this.surname = surname;
+        this.name = name;
+    }
 }
